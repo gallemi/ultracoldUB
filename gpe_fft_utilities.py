8fe24e70c50678d30f3afa6b8098ce4d235fe2b2
antoniommateo <ammateo67@gmail.com>
2016-07-07 13:20:14 +0200

    new version of utilities python shell to be used along the gp solver version 1

--- gpe_fft_utilities.py
+++ gpe_fft_utilities.py
@@ -1,13 +1,30 @@
 
 # coding: utf-8
-
+#import matplotlib
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.linalg as lin
 
 # Utilities
 # -------------------------------------------------------------------------------------
 
 # In[1]:
+
+def normaliza(c): # normalization to 1
+    norm = lin.norm(c)
+#    if ((norm-1.0)>1.0e-4): # check norm
+#        print("normalization from: ",norm)
+    return c/norm
+
+
+def normaliza_print(c): # normalization to 1
+    norm = lin.norm(c)
+    if ((norm-1.0)>1.0e-4): # check norm
+        print("normalization from: ",norm**2)
+    return c/norm  
+#
+# In[ ]:
 
 def changeFFTposition(f,N,j):  # change the order in vectors from FFT
 #    
@@ -56,14 +73,41 @@ def plot_convergence(x,y,n):
 
 def plot_density(z,psi,Lz,t):
     f2=plt.figure()
-    plt.title('Final state at $t \,\\omega_{ho}=%g$'%(t),fontsize=15)
+    plt.title('State at $t \,\\omega_{ho}=%g$'%(t),fontsize=15)
     plt.xlabel('$x/a_{ho}$',fontsize=15)
     #plt.ylabel('$\\psi\,(x)$',fontsize=15)
     #plt.axis([-Zmax,Zmax,0, 8])
     plt.xticks(np.arange(-Lz, Lz+1,Lz/2))
     plt.locator_params('y',nbins=3)
-    #plt.plot(z, psi.real, 'r.',label='real$(\psi)$')
-    #plt.plot(z, psi.imag, 'b--',label='imag$(\psi)$')
     plt.plot(z, abs(psi)**2, 'b--',label='$|\psi|^2$') # plot density
     plt.legend(fontsize=15)
     f2.show()
+
+# In[ ]:
+
+def plot_phase(z,psi,Lz,t):
+    f3=plt.figure()
+    plt.title('State at $t \,\\omega_{ho}=%g$'%(t),fontsize=15)
+    plt.xlabel('$x/a_{ho}$',fontsize=15)
+    #plt.ylabel('$\\psi\,(x)$',fontsize=15)
+    #plt.axis([-Zmax,Zmax,0, 8])
+    plt.xticks(np.arange(-Lz, Lz+1,Lz/2))
+    plt.locator_params('y',nbins=3)
+    plt.plot(z, np.angle(psi), 'b.',label='$Arg(\psi)$')
+    plt.legend(fontsize=15)
+    f3.show()
+    
+# In[ ]:
+
+def plot_real_imag(z,psi,Lz,t):
+    f3=plt.figure()
+    plt.title('State at $t \,\\omega_{ho}=%g$'%(t),fontsize=15)
+    plt.xlabel('$x/a_{ho}$',fontsize=15)
+    #plt.ylabel('$\\psi\,(x)$',fontsize=15)
+    #plt.axis([-Zmax,Zmax,0, 8])
+    plt.xticks(np.arange(-Lz, Lz+1,Lz/2))
+    plt.locator_params('y',nbins=3)
+    plt.plot(z, psi.real, 'r.',label='real$(\psi)$')
+    plt.plot(z, psi.imag, 'b--',label='imag$(\psi)$')
+    plt.legend(fontsize=15)
+    f3.show()    
